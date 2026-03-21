b048c03a40ecceb0bc7c239f1768190408b92e77
itayLev-Ops-Playground <itay.leb@gmail.com>
2026-03-21 12:02:25 -0700

    Fix error handling to work correctly

--- logcheck.py
+++ logcheck.py
@@ -4,20 +4,22 @@ def check_log():
     
     This function asks the user for a logs file name and prints all lines with ERROR status 
     """
-
+    
+    log = ""
+    
     try:
         file_name = input("Enter a file name\n")
         file_path = f"{f"./{file_name}"}"
         
         with open(file_path, 'r') as log_file:
             log = log_file.read()
-        
-        log_lines = log.split("\n")
-        
-        for line in log_lines:
-            if "ERROR" in line:
-                print("Error:", line)
 
     except FileNotFoundError as e:
-        print(f"Error: The file '{file_path}' was not found.")
-        print(f"An error occurred: {e.strerror}")
+    print(f"Error: The file '{file_path}' was not found.")
+    print(f"An error occurred: {e.strerror}")
+        
+    log_lines = log.split("\n")
+        
+    for line in log_lines:
+        if "ERROR" in line:
+            print("Error:", line)
