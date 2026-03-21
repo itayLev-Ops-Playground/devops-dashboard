ed3adfc37646d6e68acb282bcdd01155af3c864e
itaylev <itaylev@campus.technion.ac.il>
2026-03-21 20:02:35 +0200

    Added docstrings to main() and show_manu()

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def show_menu():
+    """
+    Show main menu
+    This function prints main menu
+    """
+
     menu = """
 1. System Info
 2. Log Checker
@@ -9,17 +14,26 @@ def show_menu():
 
 is_main_menu_on = True
 
-while is_main_menu_on:
-    show_menu()
+def main():
 
-    user_choice = int(input("Enter choice then press enter: "))
+    while is_main_menu_on:
+        """
+        Main menu logic
 
-    if user_choice == 1:
-        print("Coming soon...")
-    if user_choice == 2:
-        print("Coming soon...")
-    if user_choice == 3:
-        print("Coming soon...")
-    if user_choice == 0:
-        print("See you soon...")
-        is_main_menu_on = False
+        This function prints main menu,
+        takes a choice from the user,
+        runs the coresponding module
+        """
+        show_menu()
+
+        user_choice = int(input("Enter choice then press enter: "))
+
+        if user_choice == 1:
+            print("Coming soon...")
+        if user_choice == 2:
+            print("Coming soon...")
+        if user_choice == 3:
+            print("Coming soon...")
+        if user_choice == 0:
+            print("See you soon...")
+            is_main_menu_on = False
