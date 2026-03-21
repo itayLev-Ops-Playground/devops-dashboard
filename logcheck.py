80c0611bcd356af3edc30435dc0e42193958dbd2
itayLev-Ops-Playground <itay.leb@gmail.com>
2026-03-21 11:59:04 -0700

    Added logcheck.py to check for ERROR lines in log file

---
+++ logcheck.py
@@ -0,0 +1,23 @@
+def check_log():
+    """
+    Look for error lines in a log
+    
+    This function asks the user for a logs file name and prints all lines with ERROR status 
+    """
+
+    try:
+        file_name = input("Enter a file name\n")
+        file_path = f"{f"./{file_name}"}"
+        
+        with open(file_path, 'r') as log_file:
+            log = log_file.read()
+        
+        log_lines = log.split("\n")
+        
+        for line in log_lines:
+            if "ERROR" in line:
+                print("Error:", line)
+
+    except FileNotFoundError as e:
+        print(f"Error: The file '{file_path}' was not found.")
+        print(f"An error occurred: {e.strerror}")
